dd430334b48e7bf3177871c42ba618b9a6c1b203
Akash <133479686+akashprasannaksd@users.noreply.github.com>
2023-12-10 18:44:14 +0530

    mini project
    mini project code uploaded

---
+++ exam_reg.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $student_id = $_POST["student_id"];
+    $semester = $_POST["semester"];
+    $subject_code = $_POST["subject_code"];
+   
+
+    $sql = "INSERT INTO exam_reg (student_id, semester, Subject_code) VALUES ('$student_id','$semester','$subject_code')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
